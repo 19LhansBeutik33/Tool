2a1335cf0e51faf120b5f99b1687db299cca5fa0
Din-zUgex95 <55273154+Din-zUgex95@users.noreply.github.com>
2019-11-01 16:02:56 +0700

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+# tool sampah gue memang jelek monyet
+# tapi segini juga gue bisa bikin nJiinkk
+# dan lo Mau kepoin tool gue ..?
+# kontol mu bangsatt
+
+clear
+b='\033[34;1m'
+g='\033[92;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m' 
+a='\033[90m'                                                 
+clear
+lnjt(){
+echo ""
+echo ""
+echo "$b[\033[93m•$b] \033[92mTunggu\033[93m ..."
+}
+sleep 1
+echo "\033[94m╔═[ \033[92mNama Anda \033[94m]"
+read -p "╚═> " nama
+clear
+sleep 2
+clear
+banner(){
+echo "$r
+✠$b──────────────────────────────────────────────────────────$r✠
+$r
+     ██████$w╗ $w Author $y: \033[96mDin-zUgex95
+ $r   ██ ██ ██$w╗$w Github $y: \033[90mhttps://github.com/Din-zUgex95
+$r    ████████$w║$p ___  _ _  _ $y,\033[95m___$c _  _ ____ ____ _  _ $r™
+    $r████  ██$w║ $p|  \ | |\ |   /  $c|  | | __ |___  \/
+    $w╚$r██████$w╔╝ $p|__/ | | \|  /__ $c|__| |__] |___ _/\_       $a))
+     $w╚═════╝                                                                                                                  
+    $w(̅_̅_̅_̅_̅_̅_̅_̅_̅_̅$r(̅(̅\033[97m_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅_̅$a()"
+ 
+echo "$r✠$b──────────────────────────────────────────────────────────$r✠"
+echo "                                        \033[90m   Hallo \033[92m$nama"
+}
+clear
+banner
+echo""
+sleep 1
+echo "\033[93m • \033[95mNOTE \033[93m: "
+sleep 0.2
+echo "\033[93m • \033[90mBila Terjadi Error Itu Tergantung Muka Kalian"
+echo "\033[93m • \033[90mTrus lah Berusaha Mengubah Muka Kalian Agar Work :)"
+echo ""
+sleep 2
+echo "\033[91m ╔════\033[93m•\033[92m Install Bahan Dulu \033[91m!!!"
+sleep 0.2
+echo "$r ╠════════════════════════════════$b✆"
+echo "$r ╚═╗"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m1$b]\033[95m►  \033[92mInstal Bahan $b[\033[91m√$b]"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m2$b]\033[95m►  \033[92mSpamCall"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m3$b]\033[95m►  \033[92mSpamSms"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m4$b]\033[95m►  \033[92mHackFb $b[\033[93m1$b]$r╗"
+echo "$r   ║                 ╠"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m5$b]\033[95m►  \033[92mHackFb $b[\033[93m2$b]$r╝"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m6$b]\033[95m►  \033[92mSmsGratis"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m7$b]\033[95m►  $c+\033[92mTombolTermux"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m8$b]\033[95m►  \033[92mBuat SC Deface"
+echo "$r   ║"
+sleep 0.2
+echo "$r   ╠═$b[\033[93m9$b]\033[95m►  \033[92mHackCCTV"
+echo "$r   ║"
+sleep 0.2 
+echo "$r   ╠═$b[\033[93m10$b]\033[95m► \033[92mVirtexWhatsApp"
+echo "$r   ║"
+echo "$r   ╠═$b[\033[93m00$b]\033[95m► \033[93mExit"
+echo "$r ╔═╝"
+sleep 0.2
+echo "$r ╚═╦══════════════════════════════$b✆"
+sleep 0.1
+echo "$r╔══╩════$b[\033[91m•\033[93m•\033[92m•$b]"$r
+read -p "╚══➢ " no
+
+if [ $no = "1" ]; then
+clear
+banner
+echo""
+sleep 1
+echo "\033[91m╔═\033[93m• \033[92mMungkin Lama Dalam Penginstalan"
+sleep 1
+echo "\033[91m╠═\033[93m• \033[92mSabar Aja Bangsad"
+sleep 2
+echo "\033[91m╚═\033[93m• \033[92mOrang Sabar Disayang janda :v"
+sleep 4
+clear
+pkg install python2 -y
+pkg install php -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+pkg install python -y
+fi
+sleep 3
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "2" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/Din-zUgex95/Call
+cd Call
+php spamcall.php
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "3" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/sixtysix-Team/bomsms
+cd bomsms
+php bomsms.php
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "4" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/storiku/darkfb
+cd darkfb
+python2 Dark.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "5" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 MBF.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "6" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/Mr-xDODOL/dark-sms
+cd dark-sms
+python2 sms.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "7" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/Din-zUgex95/BTN
+cd BTN
+python tombol.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "8" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "9" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/storiku/HackCCTV
+cd HackCCTV
+python2 HCCTV.py
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "10" ];then
+clear
+lnjt
+sleep 2
+clear
+git clone https://github.com/Mr-xDODOL/VIRTEX
+cd VIRTEX
+sh virtex.sh
+fi
+#==========PEMBATAS DOANG MONYET===========
+if [ $no = "00" ];then
+clear
+echo ""$r
+
+figlet  EXIT
+echo "$b✵ \033[93mTerima Kasih \033[92m$nama\033[93m Telah Gunakan Tool Ini"
+echo ""
+echo ""
+sleep 3
+exit
+fi
+if [ $no = "123" ];then
+python2 dkfb.py
+fi
